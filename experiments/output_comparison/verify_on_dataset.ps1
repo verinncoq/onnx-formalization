e4c3cf2ec0ae3d602ad07382247194e9a6a66d61
Zawuza <andreizawuza@gmail.com>
2025-12-29 15:34:37 +0100

    Merge statistics and experiments

---
+++ experiments/output_comparison/verify_on_dataset.ps1
@@ -0,0 +1,6 @@
+#Should be executed in "experiments" directory
+cd ../../scripts
+foreach ($onnx in Get-ChildItem "../experiments/output_comparison/dataset" -Filter "*.onnx") {
+    ./convert.ps1 -net $onnx.BaseName -file $onnx.FullName -evaluations 100
+}
+cd ../../experiments
