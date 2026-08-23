1d9b6c43b40b0010ab57b96e406ffeb7d0e91071
Zawuza <andreizawuza@gmail.com>
2026-08-23 21:26:34 +0200

    v 4.4

--- install_rocq_platform.sh
+++ install_rocq_platform.sh
@@ -19,7 +19,7 @@ expect "Install full"                    { send "b\r" }
 expect "Select package list"             { send "1\r" }
 expect "Build opam packages parallel"    { send "s\r" }
 expect "Number of parallel make jobs"   { send "1\r" }
-expect "## Where should it be installed ? [/usr/local/bin]" { send "\r" }
+expect "## Where should it be installed ?" { send "\r" }
 expect "Install non open source SW CompCert" { send "n\r" }
 expect "Include (i) exclude (e) or select (s) large packages" { send "e\r" }
 expect "Install VST"                     { send "n\r" }
