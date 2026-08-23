42baaea1c02e5dd652d27b7ec1a3f00f7dac101c
Zawuza <andreizawuza@gmail.com>
2026-08-23 21:15:38 +0200

    v3.1

--- install_rocq_platform.sh
+++ install_rocq_platform.sh
@@ -16,16 +16,16 @@ cd platform-2026.07.0
 expect <<'EOF'
 spawn ./coq_platform_make.sh
 expect {
-  "Install full" { send "b\r"; exp_continue }
-  "Select package list" { send "1\r"; exp_continue }
-  "Build opam packages parallel" { send "p\r"; exp_continue }
-  "Number of parallel make jobs" { send "4\r"; exp_continue }
-  "Install non open source SW CompCert" { send "n\r"; exp_continue }
-  "Include (i) exclude (e) or select (s) large packages" { send "e\r"; exp_continue }
-  "Install VST" { send "n\r"; exp_continue }
-  "\[1/2/3/4\]" { send "1\r"; exp_continue }
-  -glob "*Do you want to continue?*" { send "Y\r"; exp_continue }
-  "Where should it be installed" { send "\r"; exp_continue }
+  "Install full" { send "b\r" }
+  "Select package list" { send "1\r" }
+  "Build opam packages parallel" { send "p\r" }
+  "Number of parallel make jobs" { send "4\r" }
+  "Install non open source SW CompCert" { send "n\r" }
+  "Include (i) exclude (e) or select (s) large packages" { send "e\r" }
+  "Install VST" { send "n\r" }
+  "\[1/2/3/4\]" { send "1\r" }
+  -glob "*Do you want to continue?*" { send "Y\r" }
+  "Where should it be installed" { send "\r" }
   eof
 }
 EOF
